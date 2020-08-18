e77b5ab92323264412213d693641f95227638e01
shaunbong <shaun.bong@outlook.com>
2020-08-18 15:54:48 +1000

    Updates

---
+++ precache-manifest.a09d9d76eb32f067bc32708cfd99c6b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9c51fb520f842164790ab4e059770e",
+    "url": "/Up-Banking/index.html"
+  },
+  {
+    "revision": "84b6151d43e8e54e49db",
+    "url": "/Up-Banking/static/css/main.6d6e155c.chunk.css"
+  },
+  {
+    "revision": "7bf8f83f19da98f8085d",
+    "url": "/Up-Banking/static/js/2.65b8a90b.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/Up-Banking/static/js/2.65b8a90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b6151d43e8e54e49db",
+    "url": "/Up-Banking/static/js/main.1c004903.chunk.js"
+  },
+  {
+    "revision": "a883ed53979ed352195a",
+    "url": "/Up-Banking/static/js/runtime-main.d89f5797.js"
+  }
+]);
